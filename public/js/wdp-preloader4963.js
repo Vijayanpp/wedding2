3cff4c273d85ad2cf6f293d318180c17fd3023ce
GUI TEAM <GUI TEAM>
2017-04-19 21:45:07 +0530

    committing the changes

---
+++ public/js/wdp-preloader4963.js
@@ -0,0 +1,11 @@
+/*
+Version : 1.1
+*/
+
+jQuery(window).load(function($){
+
+    "use strict";
+
+    /* Hide preloader after all contents are loaded */
+    jQuery('.wdp_preloaders').addClass('animated fadeOutDown');
+});
